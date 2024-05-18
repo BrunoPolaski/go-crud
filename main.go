835ce6aba3998cf240d28f14c9761d34dddc58fc
BrunoPolaski <polaskibruno03@gmail.com>
2024-05-18 13:22:04 -0300

    feat: creating project logger

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 
+	"github.com/BrunoPolaski/go-crud/src/configuration/logger"
 	"github.com/BrunoPolaski/go-crud/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Starting the application")
+
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,24 +2,29 @@ package controller
 
 import (
 	"fmt"
-	"log"
 
+	"github.com/BrunoPolaski/go-crud/src/configuration/logger"
 	"github.com/BrunoPolaski/go-crud/src/configuration/validation"
 	"github.com/BrunoPolaski/go-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Initiating CreateUser controller")
+	logger.Info("Creating user",
+		zap.String("method", "CreateUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Println("Error trying to bind JSON")
+		logger.Error("Error while binding JSON", err)
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
+	logger.Info(fmt.Sprintf("User created: %v", userRequest),
+		zap.String("method", "CreateUser"),
+	)
 }
