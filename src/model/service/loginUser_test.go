5f199542ade9adc65bb7d0ff62fe8c3dc0527abf
BrunoPolaski <polaskibruno03@gmail.com>
2024-07-24 20:18:47 -0300

    feat: login tests completed

--- src/model/service/loginUser_test.go
+++ src/model/service/loginUser_test.go
@@ -11,69 +11,98 @@ import (
 
 func TestUserService_LoginUser(t *testing.T) {
 	ctrl := gomock.NewController(t)
-	repository := mocks.NewMockUserRepository(ctrl)
-	service := NewUserDomainService(repository)
-	user := mocks.NewMockUserDomainInterface(ctrl)
+	mockRepository := mocks.NewMockUserRepository(ctrl)
+	service := NewUserDomainService(mockRepository)
+	mockUser := mocks.NewMockUserDomainInterface(ctrl)
 
 	t.Run("shall_return_success_when_credentials_are_valid", func(t *testing.T) {
-		user.EXPECT().GetEmail().Return("email")
-		user.EXPECT().GetPassword().Return("password")
+		mockUser.EXPECT().GetEmail().Return("email")
+		mockUser.EXPECT().GetPassword().Return("password")
 
-		repository.EXPECT().FindUserByEmailRepository(user.GetEmail()).Return(
-			user,
+		mockRepository.EXPECT().FindUserByEmailRepository(mockUser.GetEmail()).Return(
+			mockUser,
 			nil,
 		)
 
-		user.EXPECT().ComparePassword(user.GetPassword()).Return(
+		mockUser.EXPECT().ComparePassword(mockUser.GetPassword()).Return(
 			nil,
 		)
 
-		user.EXPECT().GenerateToken().Return(
+		mockUser.EXPECT().GenerateToken().Return(
 			"token",
 			nil,
 		)
 
-		user.EXPECT().GetEmail().Return("email")
-		user.EXPECT().GetPassword().Return("password")
+		mockUser.EXPECT().GetEmail().Return("email")
+		mockUser.EXPECT().GetPassword().Return("password")
 
-		response, token, err := service.LoginUserService(user)
+		response, token, err := service.LoginUserService(mockUser)
 
 		assert.Nil(t, err)
 		assert.NotNil(t, response)
 		assert.NotNil(t, token)
 		assert.Equal(t, "token", token)
-		assert.Equal(t, user, response)
+		assert.Equal(t, mockUser, response)
 	})
 
 	t.Run("shall_return_error_when_user_not_found", func(t *testing.T) {
-		user.EXPECT().GetEmail().Return("email")
-		repository.EXPECT().FindUserByEmailRepository(user.GetEmail()).Return(
+		mockUser.EXPECT().GetEmail().Return("email")
+		mockRepository.EXPECT().FindUserByEmailRepository(mockUser.GetEmail()).Return(
 			nil,
-			rest_err.NewNotFoundError("User not found"),
+			rest_err.NewNotFoundError("mockUser not found"),
 		)
 
-		user.EXPECT().GetEmail().Return("email")
+		mockUser.EXPECT().GetEmail().Return("email")
 
-		user, token, err := service.LoginUserService(user)
+		mockUser, token, err := service.LoginUserService(mockUser)
 
-		assert.Nil(t, user)
+		assert.Nil(t, mockUser)
 		assert.NotNil(t, err)
 		assert.Equal(t, token, "")
 	})
 
-	t.Run("shall_return_error_when_user_not_found", func(t *testing.T) {
-		user.EXPECT().GetEmail().Return("email")
-		repository.EXPECT().FindUserByEmailRepository(user.GetEmail()).Return(
+	t.Run("shall_return_error_when_passwords_mismatch", func(t *testing.T) {
+		mockUser.EXPECT().GetEmail().AnyTimes().Return("email")
+		mockUser.EXPECT().GetPassword().AnyTimes().Return("password")
+		mockRepository.EXPECT().FindUserByEmailRepository("email").Return(
+			mockUser,
 			nil,
-			rest_err.NewNotFoundError("User not found"),
 		)
 
-		user.EXPECT().GetEmail().Return("email")
+		mockUser.EXPECT().ComparePassword("password").Return(
+			rest_err.NewUnauthorizedError("Invalid credentials"),
+		)
 
-		user, token, err := service.LoginUserService(user)
+		mockUser, token, err := service.LoginUserService(mockUser)
 
-		assert.Nil(t, user)
+		assert.Nil(t, mockUser)
 		assert.NotNil(t, err)
 		assert.Equal(t, token, "")
+		assert.Equal(t, err.Error(), "Invalid credentials")
+	})
+
+	t.Run("shall_return_error_when_not_generating_token", func(t *testing.T) {
+		mockUser.EXPECT().GetEmail().AnyTimes().Return("email")
+		mockUser.EXPECT().GetPassword().AnyTimes().Return("password")
+		mockRepository.EXPECT().FindUserByEmailRepository("email").Return(
+			mockUser,
+			nil,
+		)
+
+		mockUser.EXPECT().ComparePassword("password").Return(
+			nil,
+		)
+
+		mockUser.EXPECT().GenerateToken().Return(
+			"",
+			rest_err.NewInternalServerError("Error trying to create jwt"),
+		)
+
+		mockUser, token, err := service.LoginUserService(mockUser)
+
+		assert.Nil(t, mockUser)
+		assert.NotNil(t, err)
+		assert.Equal(t, token, "")
+		assert.Equal(t, err.Error(), "Error trying to create jwt")
 	})
 }
